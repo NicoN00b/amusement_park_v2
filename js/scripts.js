1a87a027fe7a1e3918b806416b4af895c3a414fd
camden swita and Nicholas Raethke <camden@>
2017-06-05 20:08:02 -0700

    Add class swapping site for amusement park

--- js/scripts.js
+++ js/scripts.js
@@ -2,8 +2,8 @@ $(document).ready(function() {
   var height = parseInt(prompt("How tall are you?"));
 
   if (height >= 60) {
-    $('#ride1, #ride2, #ride4').show();
+    $('#ride1, #ride2, #ride4').addClass("highlight");
   } else {
-    $('#ride3, #ride5').show();
+    $('#ride3, #ride5').addClass("highlight");
   }
 });
